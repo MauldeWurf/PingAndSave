dbfa625887cba4f9a466cf2299303043760ca02f
ste vogt <ste vogt>
2017-03-09 13:38:36 +0100

    added timeout in ping for faser scan

--- pingAndSave.ps1
+++ pingAndSave.ps1
@@ -1,7 +1,7 @@
 $OutputFile = "C:\temp\ping4.txt";
 for ($i=0;$i -lt 255; $i++){
 "Portnumber: " + $i + ": send ping....";
-$OnePing = ping -n 1 192.168.2.$i;
+$OnePing = ping -n 1 -w 300 192.168.2.$i;
 if ($OnePing -like '*ms*'){
 $OnePing | Out-File -append $OutputFile; "positive answer at: " + $OnePing}
 {no answer}
