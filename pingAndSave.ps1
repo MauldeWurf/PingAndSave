d02ce5b621c3c05a0dd8790256b08a61095bb746
MauldeWurf <stevogt@gmail.com>
2017-04-03 21:44:17 +0200

    use webclient distinguish different types of devices in my network

--- pingAndSave.ps1
+++ pingAndSave.ps1
@@ -3,10 +3,11 @@
 
 $OutputFile = "C:\temp\PingIPRange.log"; #File to save the responding IPs
 $IPAdressRoot = "192.168.2." #part of network that should be scanned i.e. "192.168.2." for "192.168.2.xxx"
-$PingTimeout = 10
+$PingTimeout = 50
 $PingNumber = 1
 $ListOfIP = New-Object System.Collections.ArrayList
 $Date = Get-Date
+$webC = New-Object System.Net.Webclient
 
 Function LogWrite
 {
@@ -14,6 +15,40 @@ Param ([string]$logstring)
 Write-Host $logstring
 $logstring | Out-File -append $OutputFile;
 }
+
+Function AskResponse
+{
+Param ([string]$IPtoContact)
+$responseString =""
+$IPtoContact = "http://" + $IptoContact
+$UriLocal = New-Object System.Uri($IPtoContact)
+Try{
+    $responseString = $webC.DownloadString($UriLocal)
+}
+Catch{
+    $ErrorMessage = $_.Exception.Message
+    return ("ErrorCode" + $ErrorMessage)
+    
+}
+return $responseString
+}
+
+Function Recognize
+{
+Param ([string]$ResponseString)
+ if($ResponseString.Contains("Speedport W 723V")){ return "Router"}
+ if($ResponseString.Contains("Canon")){ return "Printer"}
+ if($ResponseString.Contains("www.w3.org/1999/xhtml")){ return "WIFI-Extender"}
+ if($ResponseString.Contains("ErrorCode")){
+    if($ResponseString.Contains("401")){ return "TP-link/authorized only"}
+    if($ResponseString.Contains("404")){ return "not responding 404"}
+    else {return "User device/not responding"}
+    }
+ else {return "???"}
+
+
+}
+
 LogWrite "`n_________________________________________________________________________`n"
 LogWrite "Starting IP-Scan...."
 LogWrite $Date
@@ -21,24 +56,29 @@ LogWrite ("Ping TimeOut: " + [string]$PingTimeout)
 LogWrite ("Ping Number of trys per IP: "+ [string]$PingNumber)
 LogWrite "`n_________________________________________________________________________`n"
 
-for ($i=0;$i -lt 256; $i++){
-$IPFull = $IPAdressRoot + $i;
-"IP Adress " + $IPFull + ": send ping....";
-$OnePing = ping -n $PingNumber -w $PingTimeout $IPFull;
-if ($OnePing -like '*ms*'){
-$OnePing | Out-File -append $OutputFile;
-"Found something!`n " + $OnePing;
-$ListOfIP.Add($IPFull);
+for ($i=0;$i -lt 256; $i++){ #for full scan use 256
+    $IPFull = $IPAdressRoot + $i;
+    "IP Adress " + $IPFull + ": send ping....";
+    $OnePing = ping -n $PingNumber -w $PingTimeout $IPFull;
+    if ($OnePing -like '*ms*'){
+    $OnePing | Out-File -append $OutputFile;
+    "Found something!`n " + $OnePing;
+    $ListOfIP.Add($IPFull);
 }
-{no answer}
+    {no answer}
 }
+
 LogWrite "`n============================================="
 LogWrite "List of IPs that answered to Ping:"
 LogWrite "=============================================`n"
+
 foreach ($IP in $ListOfIP)
 {
-LogWrite $IP;
+    $ResponseString = AskResponse $IP
+    $DeviceName = Recognize($ResponseString)
+    LogWrite ($IP + ": " + $DeviceName)
 }
+
 LogWrite ("`nNumber of Elements found: " + $ListOfIP.Count)
 LogWrite "_____________________________________________`n"
 
